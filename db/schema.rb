be43cf512734b23141f8677a849900a263cad6e7
bshelor <bryson_shelor@taylor.edu>
2016-09-20 04:37:09 +0000

    Created User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311172013) do
+ActiveRecord::Schema.define(version: 20160920041356) do
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe User do
     let(:user) { User.create(name: 'John Doe', email: 'jdoe@example.com', password: 'password') }
