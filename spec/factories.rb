b321361cb1f6642e1200105d98cf66c67843c734
Bryson Shelor <bryson_shelor@taylor.edu>
2016-12-05 17:25:46 +0000

    updated factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,45 @@ FactoryGirl.define do
 	sequence(:email) { |i| "user.#{i}@example.com" }
 	password 'password'
 	password_confirmation 'password'
+
+	factory :admin do
+	    admin true
+	end
+    end
+
+    factory :church do
+      user
+   
+
+
+	transient { num_services 1 }
+
+	after(:create) do |church, evaluator|
+	    create_list(:service, evaluator.num_services, church: church)
+	end
+    end
+
+    factory :service do
+	church
+	transient { num_rides 1 }
+
+	after(:create) do |service, evaluator|
+	    create_list(:ride, evaluator.num_rides, service: service)
+	end
+    end
+
+    factory :ride do
+    	user
+    	service
+	transient { num_riders 1 }
+
+	after(:create) do |ride, evaluator|
+	    ride.users = create_list(:user, evaluator.num_riders)
+	end
+    end
+
+    factory :user_ride do
+      user
+      ride
     end
 end
