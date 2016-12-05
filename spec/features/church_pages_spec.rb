375b3592dc83a2449c6fde508605417185854624
Bryson Shelor <bryson_shelor@taylor.edu>
2016-12-05 17:10:25 +0000

    updated files

--- spec/features/church_pages_spec.rb
+++ spec/features/church_pages_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "Church Pages" do
     subject { page }
--- spec/models/church_spec.rb
+++ spec/models/church_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Church do
     let(:church) { FactoryGirl.create(:church) }
